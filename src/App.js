150d901f7c19160130b9148793929681ac1aa583
Andriambololomanana Brondone <andriambololomananabrondone@gmail.com>
2023-11-14 14:01:41 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold text-green-500">
         Hello world!
       </h1>
     </div>
